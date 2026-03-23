3307f80395d88c36b48d5b1df9261c7edbfea920
Smash42 <228090741+Smash42@users.noreply.github.com>
2026-03-23 15:10:19 -0400

    styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -29,11 +29,14 @@ export default function Index() {
     );
 
   return (
-    <View className="flex-1 justify-center bg-red-100">
-      <Text className="text-4xl text-center text-emerald-500">
+    <View className="flex-1 justify-center bg-slate-300">
+      <Text className="text-4xl text-center text-emerald-800">
         Tierney's API List App
       </Text>
-      <Text className="text-2xl "> Harry Potter Characters</Text>
+      <Text className="text-2xl font-bold underline pl-5 mt-5">
+        {" "}
+        Harry Potter Characters
+      </Text>
       <Text> </Text>
       {dataLoading ? (
         <ActivityIndicator />
@@ -48,9 +51,13 @@ export default function Index() {
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
             <View>
-              <Text className="text-xl items-center">
+              <Text className="text-xl items-center font-medium">
                 {" "}
-                - {item.name}: {item.house || "Unknown"}
+                - {item.name}
+              </Text>
+              <Text className="text-lg pl-7">
+                {" "}
+                House: {item.house || "Unknown"}{" "}
               </Text>
               <Text> {} </Text>
             </View>
